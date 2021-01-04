7e22d1a5914688c23b33fd682ad2e7df23512e39
syeda-sumreen-ali <sumreenali111@gmail.com>
2021-01-04 15:41:21 +0500

    adding chat ui

--- mobile/screen/HomeScreen.js
+++ mobile/screen/HomeScreen.js
@@ -1,34 +1,83 @@
 import React, { useEffect, useState, useRef } from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { GiftedChat } from 'react-native-gifted-chat'
 import io from "socket.io-client"
 const HomeScreen = () => {
 
 	const [messageToSend, setMessageToSend] = useState("");
 	const [recvMessages, setRecvMessages] = useState([]);
-	const socket = useRef(null)
+	const socket = useRef(null);
 
 	useEffect(() => {
-		socket.current = io("http://192.168.0.104:3001")
+		//connect react native with socket.io
+		socket.current = io("http://192.168.7.146:4000")
+		setRecvMessages([
+			//dummy data of messages for testing ui
+			{
+				_id: 1,
+				text: 'Hello developer',
+				createdAt: new Date(),
+				user: {
+					_id: 2,
+					name: 'React Native',
+					avatar: 'https://placeimg.com/140/140/any',
+				},
+			},
+			{
+				_id: 2,
+				text: 'Hello from myself!',
+				createdAt: new Date(),
+				user: {
+					_id: 1,
+					name: 'React Native 2',
+					avatar: 'https://placeimg.com/140/140/any',
+				},
+			},
+		])
+
+		//receiving messages from socket.io  and appending it in an array
 		socket.current.on("message", message => {
-			setRecvMessages(prevState => [...prevState, message]);
-		})
+			// adding dummy data to fulfil the object requirements of gifted ui message
+			const testMessage = {
+				_id: 3,
+				text: 'Hello developer',
+				createdAt: new Date(),
+				user: {
+					_id: 2,
+					name: 'React Native',
+					avatar: 'https://placeimg.com/140/140/any',
+				},
+			};
+
+			testMessage.text = message
+
+			// setRecvMessages(prevState => [...prevState, testMessage]);
+			setRecvMessages(prevState => GiftedChat.append(prevState, testMessage))
+		});
+
+
 	}, []);
 
-	const sendMessage = () => {
-		socket.current.emit("message", messageToSend);
-		setMessageToSend("");
+	// sending message to socket.io 
+	const sendMessage = messages => {
+
+		console.log(messages[0].text)
+		socket.current.emit("message", messages[0].text);
+		// setRecvMessages(prevState => GiftedChat.append(prevState, testMessage))
+		// setMessageToSend("");
 	}
 
-	const textOfRecvMessages = recvMessages.map(msg => (<Text key={msg}>{msg}</Text>));
+
+	// const textOfRecvMessages = recvMessages.map(msg => (<Text key={msg}>{msg}</Text>));
 
 	return (
-		<View>
-			{textOfRecvMessages}
-			<TextInput
-				placeholder="Enter chat message..."
-				value={messageToSend}
-				onChangeText={(val) => setMessageToSend(val)}
-				onSubmitEditing={sendMessage}
+		<View style={{ flex: 1 }}>
+			<GiftedChat
+				messages={recvMessages}
+				onSend={messages => sendMessage(messages)}
+				user={{
+					_id: 1,   // user own id
+				}}
 			/>
 		</View>
 	)
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,5 +1,7 @@
 const io = require("socket.io")();
+console.log("app is running...");
 io.on("connection", socket => {
+	// console.log(socket)
 	console.log("user is connected...");
 	socket.on("message", message => {
 		console.log(message);
@@ -7,4 +9,8 @@ io.on("connection", socket => {
 	})
 });
 
-io.listen(3001);
+// io.on("connection", function () {
+// 	console.log("user is connected")
+// })
+
+io.listen(4000);
